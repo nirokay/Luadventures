9360cadba206efe69f1de22a553c8216c4a823fe
NiroUwU <rostiplotni@gmail.com>
2022-12-04 04:34:44 +0100

    added farming-turtle script
    added farming script
    reworked images shenanigans stuff

--- CC: Tweaked/README.md
+++ CC: Tweaked/README.md
@@ -1,31 +1,44 @@
 # What is this?
+
 This is a collection of lua scripts made in "ComputerCraft: Tweaked", a Minecraft Mod.
 
 https://www.curseforge.com/minecraft/mc-mods/cc-tweaked
 
-
 ## Contents
+
 If you see a...
+
 * ?[] next to the name, another mod/addon is needed for the full script "experience", but not required
 * ![] next to the name, another mod/addon is required for the script
 
 Additional mods/addons:
-* [AP] Advanced Peripherals: https://www.curseforge.com/minecraft/mc-mods/advanced-peripherals
 
+* [AP] [Advanced Peripherals](https://www.curseforge.com/minecraft/mc-mods/advanced-peripherals)
 
 #### Note: You can always rewrite/comment out chunks of the code to make it work withoud additional mods!
 
 ### Display Time
+
 A script that displays the current Minecraft and Server time to a monitor.
 
-![Example](/pictures/displaytime.png "Example")
+![displaytime.png](.github/img/displaytime.png)
+
+### Farming Turtle
+
+Put this script on a mining turtle and enjoy your new ~~slave~~ helper!
+
+This is my first actually useful AND (somewhat) well-written script!
+
+![farming.jpg](.github/img/farming.jpg)
 
 ### Reactor Control ?[AP]
+
 A script I made to observe the state of a nuclear reactor (Mod: Mekanism), warning us if the reactor goes offline.
 
-![Example](/pictures/reactor0.png "Example")
+![reactor0.png](.github/img/reactor0.png)
 
-![Example](/pictures/reactor1.png "Example")
+![reactor1.png](.github/img/reactor1.png)
 
 ### Time Reminder ![AP]
+
 A script to send chat messages on certain times (real time, not minecraft time) to remind of some events (for example: server restart).
---
+++ CC: Tweaked/farming.lua
@@ -0,0 +1,206 @@
+-------------
+-- CONFIG: --
+-------------
+
+local config = {
+    -- Sleeping in seconds:
+    sleep = {
+        forRefuel = 5,                            -- Default:  10
+        afterEmptying = 180                       -- Default: 180
+    },
+
+    -- Full name of crop as item and block and ripeness:
+    crop = {
+        block = "minecraft:potatoes",
+        item  = "minecraft:potato",
+        ripe  = 7                                 -- Default: 7 (crops have grow stages between 0 and 7)
+    },
+
+    -- Blocks to listen to for instructions on how to move/interact:
+    blocks = {
+        forward   = "minecraft:white_wool",       -- optional (turtle will move forwards each step regardless, only useful for debugging)
+        turnRight = "minecraft:green_wool",       -- required
+        turnLeft  = "minecraft:blue_wool",        -- required
+
+        emptyInventory = "minecraft:hopper",      -- required
+        refuelTurtle   = "minecraft:yellow_wool"  -- optional *(removing will require manual refueling)*
+    },
+
+    -- Print out debug information on what the turtle is currently doing:
+    debug = false                                 -- Default: false
+}
+
+
+----------------
+-- VARIABLES: --
+----------------
+
+-- Function getting called the next step:
+local BufferFunction = nil
+
+-- Dictionary of block names and its effects on the turtle:
+local BlockList = {}
+
+
+----------------
+-- FUNCTIONS: --
+----------------
+
+-- Function to print debug information: (with and without newline)
+local function debug_write(txt, ...)
+    if not config.debug then return end
+    io.write(string.format(txt, ...))
+end
+local function debug_print(txt, ...)
+    debug_write(txt.."\n", ...)
+end
+
+-- Print out current config at start:
+local function printWelcomeMessage()
+    -- Clear screen:
+    term.clear()
+    term.setCursorPos(1, 1)
+    -- Print text:
+    local text = {
+        "Welcome to the farming program!",
+        "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯",
+        "  Author: nirokay",
+        "  Repo: https://github.com/nirokay\n",
+
+        "Current config:",
+        "  Crop: " .. config.crop.item,
+        "  Idle & Refuel time: " .. config.sleep.afterEmptying .. "s and " .. config.sleep.forRefuel .. "s",
+        "  Debug printout: " .. tostring(config.debug)
+    }
+    print(table.concat(text, "\n"))
+    sleep(2)
+end
+
+
+-- Add blocks to check for to BlockList dictionary:
+local function newBlock(blockName, blockFunction)
+    local temp = {
+        block = blockName,
+        fn = blockFunction
+    }
+    BlockList[blockName] = blockFunction
+    return temp
+end
+
+
+--------------------
+-- TURTLE BLOCKS: --
+--------------------
+
+-- Movement:
+newBlock(config.blocks.forward, function(_)
+    debug_print("Moving forwards...")
+    -- Empty because move forward is called after every step.
+end)
+newBlock(config.blocks.turnRight, function(_)
+    debug_print("Turning right...")
+    turtle.turnRight()
+end)
+newBlock(config.blocks.turnLeft, function(_)
+    debug_print("Turning left...")
+    turtle.turnLeft()
+end)
+
+-- Inventory:
+newBlock(config.blocks.emptyInventory, function(_)
+    debug_print("Emptying inventory...")
+    for i = 16, 1, -1 do
+        turtle.select(i)
+        turtle.dropDown()
+    end
+    debug_print(" > Going to sleep for %s seconds!", tostring(config.sleep.afterEmptying))
+    sleep(config.sleep.afterEmptying)
+    debug_print(" > Done sleeping.")
+end)
+
+-- Refueling:
+newBlock(config.blocks.refuelTurtle, function(_)
+    -- Sleep to allow items to get input through a hopper:
+    debug_print("Refueling, going to sleep for %s seconds.", config.sleep.forRefuel)
+    sleep(config.sleep.forRefuel)
+
+    -- Cycle through inventory and refuel:
+    BufferFunction = function()
+        debug_print(" > Done sleeping, refueling...")
+        for i = 16, 1, -1 do
+            turtle.select(i)
+            turtle.refuel()
+        end
+        debug_print(" > Finished refueling!")
+    end
+end)
+
+-- Harvesting and Replanting:
+local function attemptReplanting()
+    local currentItem = turtle.getItemDetail()
+    -- Place potato from currently held item:
+    if currentItem.name == config.crop.item then
+        debug_print("Placing down crop!")
+        turtle.placeDown()
+        return
+    end
+
+    -- Find potato in inventory and place it down: (more expensive on computation, please do not happen in praxis)
+    debug_print("Locating crop to replant...")
+    for i = 1, 16 do
+        turtle.select(i)
+        local newItem = turtle.getItemDetail()
+        if newItem.name == config.crop.item then
+            debug_print("Placing down crop!")
+            turtle.placeDown()
+            return
+        end
+    end
+
+    -- Failed to replant:
+    debug_print("Could not locate crop in inventory to replant... carrying on.")
+end
+newBlock(config.crop.block, function(block)
+    debug_write("Crop found... ")
+    if block.state.age >= config.crop.ripe then
+        debug_print("ready to harvest!")
+        turtle.digDown()
+        attemptReplanting()
+    else
+        debug_print("still growing...")
+    end
+end)
+
+
+-----------
+-- MAIN: --
+-----------
+
+local function main()
+    -- Execute Buffer function from last step:
+    if BufferFunction ~= nil then
+        debug_print("Executing buffer function!")
+        BufferFunction()
+        BufferFunction = nil
+    end
+
+    -- Check for the current block underneath turtle:
+    local isBlock, block = turtle.inspectDown()
+    if isBlock and block ~= nil then
+        -- Execute Block function:
+        if BlockList[block.name] ~= nil then
+            BlockList[block.name](block)
+        end
+    end
+
+    -- Move forward: (gets called after every block check, because yeah... stuff)
+    turtle.forward()
+end
+
+
+printWelcomeMessage()
+while true do
+    main()
+end
+
+
